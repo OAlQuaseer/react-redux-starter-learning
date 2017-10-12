72e6001bdb7155507bf290df96f07afd0924142b
Omar AlQuaseer <AlQuaseer@Omars-MacBook-Pro.local>
2017-10-12 15:59:49 -0500

    first commit.
    setting up the scaffolding structure using React + Redux + Hmr + Eslint
    + Webpack + Mocha.

---
+++ tools/endMessage.js
@@ -0,0 +1,5 @@
+import colors from 'colors';
+
+/* eslint-disable no-console */
+
+console.log('WELCOME TO DMA APPLICATION'.green);
---
+++ tools/startMessage.js
@@ -0,0 +1,5 @@
+import colors from 'colors';
+
+/* eslint-disable no-console */
+
+console.log('Starting DMA app in dev mode...'.green);
